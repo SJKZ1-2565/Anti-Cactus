41f28641a73845b17596c761e2e00ce6955e6bdb
Taratorn Kuakorboon <66944895+SJKZ1-2565@users.noreply.github.com>
2025-05-06 15:20:30 +0700

    Move namespace from minecraft to anti-cactus

--- src/main/java/com/sjkz1/AntiCactus.java
+++ src/main/java/com/sjkz1/AntiCactus.java
@@ -14,7 +14,7 @@ public class AntiCactus implements ModInitializer
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final TagKey<Item> IMMUNE_TO_CACTUS = TagKey.create(Registries.ITEM, ResourceLocation.withDefaultNamespace("immune_to_cactus"));
+    public static final TagKey<Item> IMMUNE_TO_CACTUS = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID,"immune_to_cactus"));
 
     @Override
     public void onInitialize()
